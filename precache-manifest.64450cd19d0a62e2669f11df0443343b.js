a4c6189cbd992030e0d97f88141e9497d811f933
Nu 's <nus@Nus-MacBook-Air.lan>
2025-01-14 17:48:58 +0700

    Updates

---
+++ precache-manifest.64450cd19d0a62e2669f11df0443343b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b927f8470f01d675704df02838706406",
+    "url": "/ReactJSLab19/index.html"
+  },
+  {
+    "revision": "d0d073b612d92032355e",
+    "url": "/ReactJSLab19/static/css/main.0cd2a454.chunk.css"
+  },
+  {
+    "revision": "e9fa72b77790e9dc52ee",
+    "url": "/ReactJSLab19/static/js/2.66383de8.chunk.js"
+  },
+  {
+    "revision": "ae8cce05c3d8075415f8a34a2bf43823",
+    "url": "/ReactJSLab19/static/js/2.66383de8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d073b612d92032355e",
+    "url": "/ReactJSLab19/static/js/main.b802bc3e.chunk.js"
+  },
+  {
+    "revision": "8a3fdba3b15a86a3cf43",
+    "url": "/ReactJSLab19/static/js/runtime-main.a90514e0.js"
+  }
+]);
